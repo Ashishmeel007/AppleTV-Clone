6cb0e39da96fe5cf8e3eb931e2f6699842abacef
Ashish Meel <ashish.meel22@gmail.com>
2022-12-19 10:53:26 +0530

    project done

--- main.js
+++ main.js
@@ -1,32 +1,63 @@
+const questions=document.querySelectorAll('.question-answer');
+   
+questions.forEach(function(question){
+ const btn = question.querySelector('.question-btn');
+ btn.addEventListener("click",function() {
+   questions.forEach(function(item){
+     if (item !== question){
+       item.classList.remove("show-text")
+     }
+   })
+   question.classList.toggle("show-text");
+ })
+})
+const questions1=document.querySelectorAll('.question-answer1');
 
-let cont=document.getElementById("movie_container");
-    let input=document.getElementById("search-inp")
+questions1.forEach(function(question){
+ const btn = question.querySelector('.question-btn');
+ btn.addEventListener("click",function() {
+   questions1.forEach(function(item){
+     if (item !== question){
+       item.classList.remove("show-text")
+     }
+   })
+   question.classList.toggle("show-text");
+ })
+})
+
+
+
+
+
+    let cont=document.getElementById("movie_container");
+    let input=document.getElementsByClassName("input")
     let btn=document.getElementById("search-btn");
     
-
     btn.addEventListener("click",() =>{
       var api="https://www.omdbapi.com/?apiKey=7c955b18&s="
     var query=input.value
     api=api+query;
     
-    fetch(api)
+     fetch(api)
       .then((requestResponse) =>{ 
       return requestResponse.json()
     })
      .then((actualData) =>{
-      fetchData=actualData.Search
+      // console.log(actualData.Search)
       displayMovie(actualData.Search)
      })
      .catch((error) =>{
-      let h2=document.createElement("h2")
-      h2.setAttribute("class","error-container")
-      h2.innerText="Movie not found!"
-      cont.append(h2)
+        return error  
+      // let h2=document.createElement("h2")
+      // h2.setAttribute("class","error-container")
+      // h2.innerText="Movie not found!"
+      // cont.append(h2)
      })
     })
     function displayMovie(data){
-      cont.innerHTML=""
+      // cont.innerHTML=null
         data.forEach((element) => {
+          console.log(data)
           let card=document.createElement("div")
           card.setAttribute("class","mov")
           let image=document.createElement("img")
@@ -36,4 +67,10 @@ let cont=document.getElementById("movie_container");
           cont.append(card)
         });
     }
+  
+    
+
+
+
+
   
